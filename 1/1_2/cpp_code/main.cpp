ac1705ea9ea8521384fe6456c5816fdd1e130b90
Amirmohammad_sharbati <amirmohammad.shn84@gmail.com>
2025-12-04 22:51:56 +0330

    Test and debug. I reach to correct code but I should improve its efficiency

--- 1/1_2/cpp_code/main.cpp
+++ 1/1_2/cpp_code/main.cpp
@@ -8,7 +8,7 @@ void dominance_counting(int l, int r);
 void add_to_seen_z(int z);
 int count_z_less_than_current(int z);
 
-// 'point' declaration -> It has five elements: which one is that (first, second, ..) by index and coordinate by x, y, z and the result:
+// 'point' declaration -> It has five elements: which one is that (first, second, ..) by index, coordinate by x, y, z and the result:
 struct Point {
     int index, x, y, z, ans;
 };
@@ -16,9 +16,9 @@ struct Point {
 // These are global variables, because they are used many times:
 vector<Point> all_points;
 vector<int> seen_z;
+int n;
 
 int main() {
-    int n;
     cin >> n;
 
     all_points.resize(n);
@@ -47,7 +47,7 @@ int main() {
     dominance_counting(0, n - 1);
 
     // At the end we print the result by order of index (so we should sort it at first by indexes):
-    sort(all_points.begin(), all_points.begin(), [](const Point &a, const Point &b) {
+    sort(all_points.begin(), all_points.end(), [](const Point &a, const Point &b) {
         return a.index < b.index;
     });
 
@@ -64,12 +64,10 @@ void dominance_counting(int l, int r) {
     dominance_counting(l, mid);
     dominance_counting(mid + 1, r);
 
-    // Main idea of algorithm (merge step):
-    vector<Point> left, right;
-    for (int i = l; i <= mid; i++)
-        left.push_back(all_points[i]);
-    for (int i = mid + 1; i <= r; i++)
-        right.push_back(all_points[i]);
+    /// Main idea of algorithm is this step (merge step):
+
+    vector<Point> left(all_points.begin() + l, all_points.begin() + mid + 1);
+    vector<Point> right(all_points.begin() + mid + 1, all_points.begin() + r + 1);
 
     // Now I should sort points based on y:
     sort(left.begin(), left.end(), [](auto &a, auto &b) {
@@ -79,25 +77,34 @@ void dominance_counting(int l, int r) {
         return a.y < b.y;
     });
 
-    // seen_z is global variable. It is necessary to reinitialize it to zero (and we can also resize it, both wit resize() function):
-    seen_z.resize((mid - l + 2), 0);
+    // seen_z is global variable. It is necessary to reinitialize it to zero (and we can also resize it, both with assign() function):
+    seen_z.assign(n + 1, 0);
 
     // We only need to iterate over right array and use BIT for finding the count of (z in left array < current z):
-    int j = l;
-    for (int i = mid + 1; i <= r; i++) {
-        while (j <= mid && left[j].y < right[i].y) {
+    int j = 0;
+    for (int i = 0; i < right.size(); i++) {
+        while (j < left.size() && left[j].y < right[i].y) {
             add_to_seen_z(left[j].z);
             j++;
         }
         right[i].ans += count_z_less_than_current(right[i].z);
     }
+
+    // At the end we should merge these two sorted arrays (right and left arrays) into all_points:
+    merge(left.begin(), left.end(), right.begin(), right.end(), all_points.begin() + l, [](const Point &a, const Point &b) {
+        return a.y < b.y;
+    });
 }
 
+// These two functions are related to the BIT (Fenwick) tree. It is amazing data structure :)
+
+// We should updata specific numbers (i += i & (-i)):
 void add_to_seen_z(int z) {
-    for (int i = z; i <= seen_z.size(); i += i & (-i))
+    for (int i = z; i < seen_z.size(); i += i & (-i))
         seen_z[i] += 1;
 }
 
+// We should sum specific numbers to reach our goal (i -= i & (-i)):
 int count_z_less_than_current(int z) {
     int sum = 0;
     for (int i = z; i > 0; i -= i & (-i))
