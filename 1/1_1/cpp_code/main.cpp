efa915c6c23bb8ddacd1d315375b3c776d8297d5
Amirmohammad_sharbati <amirmohammad.shn84@gmail.com>
2025-12-08 13:52:45 +0330

    Final correct implementaion of Question 1 of Assignment 1

--- 1/1_1/cpp_code/main.cpp
+++ 1/1_1/cpp_code/main.cpp
@@ -11,26 +11,26 @@ int main() {
     cin >> n;
 
     // We need to maximize the weighted sums of first i numbers (sum(a1 to ai)): coefficients are (bi+1 - bi), so they are +-1 or 0
-    vector<int> sums(n); // sums of first i numbers
-    int median;
+    long long sum; // sums of first i numbers
 
     // We need two heaps to have the numbers with coefficients +1 and -1
-    priority_queue<int> max_heap;
-    priority_queue<int, vector<int>, greater<int>> min_heap; // Min-heap
+    priority_queue<long long> max_heap;
+    priority_queue<long long, vector<long long>, greater<long long>> min_heap; // Min-heap
 
-    int result = 0;
-    int max_heap_sum = 0;
-    int min_heap_sum = 0;
+    float median;
+    long long result = 0;
+    long long max_heap_sum = 0;
+    long long min_heap_sum = 0;
 
-    cin >> sums[0];
-    if (sums[0] >= 0) {
-        result += sums[0];
-        min_heap.push(sums[0]);
-        min_heap_sum += sums[0];
+    cin >> sum;
+    if (sum >= 0) {
+        result += sum;
+        min_heap.push(sum);
+        min_heap_sum += sum;
     } else {
-        result -= sums[0];
-        max_heap.push(sums[0]);
-        max_heap_sum += sums[0];
+        result -= sum;
+        max_heap.push(sum);
+        max_heap_sum += sum;
     }
 
     cout << result << " ";
@@ -39,7 +39,7 @@ int main() {
         int a;
         cin >> a;
 
-        sums[i] = a + sums[i - 1];
+        sum += a;
 
         if (max_heap.size() == min_heap.size()) {
             median = (max_heap.top() + min_heap.top()) / 2;
@@ -49,12 +49,12 @@ int main() {
             median = min_heap.top();
         }
 
-        if (sums[i] <= median) {
-            max_heap.push(sums[i]);
-            max_heap_sum += sums[i];
+        if (sum <= median) {
+            max_heap.push(sum);
+            max_heap_sum += sum;
         } else {
-            min_heap.push(sums[i]);
-            min_heap_sum += sums[i];
+            min_heap.push(sum);
+            min_heap_sum += sum;
         }
 
         // Rebalance heaps
