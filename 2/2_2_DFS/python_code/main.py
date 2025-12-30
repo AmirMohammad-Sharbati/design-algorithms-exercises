7877de639702c70992eab52a2536941b6ccb5f64
Amirmohammad_sharbati <amirmohammad.shn84@gmail.com>
2025-12-30 16:58:54 +0330

    Complete python code

--- 2/2_2_DFS/python_code/main.py
+++ 2/2_2_DFS/python_code/main.py
@@ -1,37 +1,30 @@
-def generate_non_descendant_sequnece (length, bound):
+def generate_non_descendant_sequence (length, bound):
     if(length == 1):
-        base_sequence = list(list())
-        for i in range(bound):
-            base_sequence.append({i+1})
+        base_sequence = [[i+1] for i in range(bound)]
         return base_sequence
 
-    prev_sequnces = generate_non_descendant_sequnece(length-1, bound)
-    final_sequences = list(list())
+    prev_sequences = generate_non_descendant_sequence(length-1, bound)
+    final_sequences = []
 
-    for sequence in prev_sequnces:
-        for i in range(sequence[-1], bound+1):
-            new_sequence = prev_sequnces
-            new_sequence.append(i)
-            final_sequences.append(new_sequence)
-    return final_sequences
-
-
-n, m, q = int(input().split(" "))
-
-constraints = list(list())
-for i in range(q):
-    a, b, c, d = int(input().split(" "))
-    constraints.append({a, b, c, d})
     
-all_non_descentant_sequences = generate_non_descendant_sequnece(n, m)
+    for sequence in prev_sequences: 
+        for i in range(sequence[-1], bound+1): 
+            final_sequences.append(sequence + [i])
+    return final_sequences
+    # return [sequence + [i] for sequence in prev_sequences for i in range(sequence[-1], bound+1)]
+
+
+n, m, q = map(int, input().split(" "))
+constraints = [list(map(int, input().split(" "))) for _ in range(q)]
+    
+all_non_descentant_sequences = generate_non_descendant_sequence(n, m)
 
 max_value = 0
 for sequence in all_non_descentant_sequences:
     value = 0
     for q in constraints:
-        if (sequence[q[1]] - sequence[q[2]] == q[3]):
-            value += q[4]
+        if (sequence[q[1]-1] - sequence[q[0]-1] == q[2]):
+            value += q[3]
     if (max_value < value): max_value = value
     
 print(max_value)
-    
